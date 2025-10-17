18c6b8f5c652a9b385b8a1b163a82d3613c772c7
yoocy0 <yoocy0328@gmail.com>
2025-10-17 17:27:18 +0900

    자바 작업물

---
+++ 10_YOOCHANYEON/chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section03/constant/Application01.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.section03.constant;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        final int AGE;
+
+        AGE = 7;
+        // AGE = 20;
+
+        System.out.println("AGE = " + AGE);
+
+        int myAge = AGE;
+        System.out.println("myAge = " + myAge);
+
+
+    }
+}
