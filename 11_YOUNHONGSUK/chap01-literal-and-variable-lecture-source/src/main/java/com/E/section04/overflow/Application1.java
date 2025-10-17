2a66b00377dc89c212e83649f6f9f6d9a803ee43
ehsy25 <eyoun17@gmail.com>
2025-10-17 17:29:24 +0900

    first commit to make pull request

---
+++ 11_YOUNHONGSUK/chap01-literal-and-variable-lecture-source/src/main/java/com/E/section04/overflow/Application1.java
@@ -0,0 +1,11 @@
+package com.E.section04.overflow;
+
+public class Application1 {
+    public static void main(String[] args){
+
+        System.out.println("byte 값의 범위 : " + Byte.MIN_VALUE + "~" + Byte.MAX_VALUE);
+        System.out.println("char 값의 범위 : " + (int) Character.MIN_VALUE + "~" + (int) Character.MAX_VALUE);
+        System.out.println("short 값의 범위 : " + Short.MIN_VALUE + "~" + Short.MAX_VALUE);
+    
+    }
+}
