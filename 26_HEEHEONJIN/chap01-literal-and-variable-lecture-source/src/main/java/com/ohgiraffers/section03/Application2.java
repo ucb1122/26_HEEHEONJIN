d77c112a098e7ba8e3cbdc71843d2311781f909d
ucb1122 <ucb1122@naver.com>
2025-10-17 17:31:26 +0900

    first commit

---
+++ 26_HEEHEONJIN/chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section03/Application2.java
@@ -0,0 +1,19 @@
+/*
+package com.ohgiraffers.section03;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        //1. 모든 문자는 영문자 대문자 혹은 숫자만 사용한다.
+
+        fianl int AGE1 = 20;
+        fianl int AGE2 = 30;
+        fianl int age3 = 40; // 소문자로 작성하는 것은 관례상 X
+
+        // 2. 단어와 단어 연결은 언더스코어(_)를 사용
+        final int MAX_AGE = 60;
+        final int MIN_AGE = 20;
+        final int minAGE = 60; // 낙타표기 관례상 x
+    }
+}
+*/
