3504e4c0e9a3d521226360951ef29949f41c11e2
rlatjddms <kseo_o@naver.com>
2025-10-20 13:31:05 +0900

    문제 풀이

---
+++ 02_KIMSEONGEUN/chap01-literal-and-variable-lecture-source/src/main/java/com/rlatjddms/section05/typecasting/Application2.java
@@ -0,0 +1,20 @@
+package com.rlatjddms.typecasting;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+        // 의도하지 않은 값손실
+//        long lNum = 8000000000L;
+        long lNum = 8;
+        int iNum = (int) lNum;
+        System.out.println("iNum = " + iNum); // 오버플로우
+        int isum = (int) (iNum + lNum);
+        int isum2 = iNum + (int) lNum;
+        long lsum = iNum + lNum;
+
+        // 의도한 값손실
+        float avg = 31.235f;
+        int floorNum = (int) avg;
+        System.out.println("floorNum = " + floorNum);
+    }
+}
