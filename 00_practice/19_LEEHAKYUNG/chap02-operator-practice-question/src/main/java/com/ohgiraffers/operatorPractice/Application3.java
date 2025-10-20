70efc8f5b035500f16096ec72fbf6bfa39606b33
fijitlwkr <hakyung1133@gmail.com>
2025-10-20 14:24:48 +0900

    solve ch2 by 19

---
+++ 00_practice/19_LEEHAKYUNG/chap02-operator-practice-question/src/main/java/com/ohgiraffers/operatorPractice/Application3.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.operatorPractice;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+
+        /*
+         * 문자형 변수 하나를 선언하고 변수에 값이 M으로 초기화하면 남자입니다.
+         * F이면 여성입니다를 출력하도록 작성하세요 (삼항연산자사용)
+         *
+         * M
+         *
+         * ---- 출력 결과 ---
+         * 남자입니다.
+         * */
+        char gender = 'F';
+        String result = (gender == 'F') ? "여성입니다." : "남성입니다.";
+        System.out.println(result);
+    }
+}
