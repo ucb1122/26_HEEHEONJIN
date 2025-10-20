4f9c882549afe040d51276a3f733296ff6e3cc46
Apeirogon99 <apeirogon99@gmail.com>
2025-10-20 18:05:19 +0900

    practice3

---
+++ 00_practice/14_GWANHO/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/basic/Application.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.level01.basic;
+
+public class Application {
+    // 실행용 클래스
+    public static void main(String[] args) {
+
+        Calculator.checkMethod();
+
+        System.out.println(Calculator.sumTwoNubmer(10, 20));
+
+        System.out.println(Calculator.multiplyTwoNubmer(10, 20));
+
+        int mul = Calculator.multiplyTwoNubmer2(10, 20);
+        System.out.println(mul);
+    }
+}
