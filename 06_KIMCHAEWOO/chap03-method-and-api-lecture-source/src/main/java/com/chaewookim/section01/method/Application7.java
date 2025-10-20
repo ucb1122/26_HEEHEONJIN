90eeb226a71467a7ffeeb54bf857490b0d0b6958
chaewookim <chaewookim2877@gmail.com>
2025-10-20 17:56:24 +0900

    problem solved

---
+++ 06_KIMCHAEWOO/chap03-method-and-api-lecture-source/src/main/java/com/chaewookim/section01/method/Application7.java
@@ -0,0 +1,21 @@
+package com.chaewookim.section01.method;
+
+public class Application7 {
+    public static void main(String[] args) {
+
+        int first = 100;
+        int second = 50;
+
+        Calculator cal = new Calculator();
+
+        // 덧셈
+        System.out.println("두 수의 합: " + cal.plusTwoNumbers(first, second));
+        // 뺄셈
+        System.out.println("두 수의 차: " + cal.subTwoNumbers(first, second));
+        // 두 수 중 큰 수
+        System.out.println("두 수 중 큰 수: " + Calculator.maxNumberOf(first, second));
+        // 두 수 중 작은 수
+        System.out.println("두 수 중 작은 수: " + Calculator.minNumberOf(first, second));
+
+    }
+}
