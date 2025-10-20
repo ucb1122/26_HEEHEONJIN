d439669c0c1ab5cf23ae59b6a8ee21397108fa5d
hathang <hathang16@naver.com>
2025-10-20 18:40:57 +0900

    문제 풀이

---
+++ 00_practice/24_JUNGHAGYEONG/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/basic/Application.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section01.level01.basic;
+
+public class Application {
+    // 실행용 클래스
+    public static void main(String[] args) {
+
+        Calculator calcultor = new Calculator();
+        calcultor.checkMethod();
+        calcultor.sumTwoNumber(10, 20);
+        calcultor.multiTwoNumber(10, 20);
+        calcultor.multiTwoNumber2(10, 20);
+
+    }
+}
