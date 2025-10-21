c3c041696ab8d3678a7ebd9d21700beb62c0f58d
이용우 <qhtm008@naver.com>
2025-10-21 14:04:59 +0900

    Practice Complete!

---
+++ 00_practice/16_LEEYONGWOO/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/level01/exampe/Application.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section01.level01.exampe;
+import java.util.Scanner;
+public class Application {
+
+    public static void main(String[] args) {
+        com.ohgiraffers.section01.level01.exampe.ExampleTest exam=new com.ohgiraffers.section01.level01.exampe.ExampleTest();
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요 : ");
+        int num=sc.nextInt();
+        exam.evenOdd(num);
+
+        System.out.print("두 정수를 입력하세요: ");
+        int first=sc.nextInt();
+        int second=sc.nextInt();
+        exam.findMax(first,second);
+
+        System.out.println("정수를 두개 입력하세요");
+        first=sc.nextInt();
+        second=sc.nextInt();
+        exam.randomMathQuiz(first,second);
+    }
+}
+
