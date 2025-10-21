a998ffe42374dd4a160a846966ff71e167a3c173
fijitlwkr <hakyung1133@gmail.com>
2025-10-21 14:19:06 +0900

    edit ch03-example

--- 00_practice/19_LEEHAKYUNG/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/example/Application.java
+++ 00_practice/19_LEEHAKYUNG/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/example/Application.java
@@ -1,9 +1,19 @@
 package com.ohgiraffers.section01.level01.example;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        ExampleTest.evenOdd(7);
-        ExampleTest.findMax(23, 101);
+        Scanner input = new Scanner(System.in);
+        System.out.print("숫자를 입력하세요: ");
+        int num1 = input.nextInt();
+        ExampleTest.evenOdd(num1);
+
+        System.out.print("크기를 비교할 두 개의 숫자를 입력하세요: ");
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+        ExampleTest.findMax(num2, num3);
+
         ExampleTest.randomMathQuiz();
     }
 }
