a1b28e87bc8dfd0a7c641274a048997ffa7a5325
fijitlwkr <hakyung1133@gmail.com>
2025-10-21 13:47:09 +0900

    solve ch03-example and update 19-ch03-lecture

---
+++ 00_practice/19_LEEHAKYUNG/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/example/Application.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section01.level01.example;
+
+public class Application {
+    public static void main(String[] args) {
+        ExampleTest.evenOdd(7);
+        ExampleTest.findMax(23, 101);
+        ExampleTest.randomMathQuiz();
+    }
+}
