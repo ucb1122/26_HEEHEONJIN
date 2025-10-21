2c3fbf16376c51e919e9db95c4850eceafdb8efc
Apeirogon99 <apeirogon99@gmail.com>
2025-10-21 18:22:48 +0900

    chap04

--- 14_GWANHO/study/src/chap04_method_api/chap04_test.java
+++ 14_GWANHO/study/src/chap04_method_api/chap04_test.java
@@ -7,8 +7,8 @@ import static chap04_method_api.test2.Calculator.*;
 public class chap04_test {
     public static void main(String[] args) {
 
-        chap04_method_api.test1.Calculator c1 = new chap04_method_api.test1.Calculator();
-        chap04_method_api.test2.Calculator c2 = new chap04_method_api.test2.Calculator();
+//        chap04_method_api.test1.Calculator c1 = new chap04_method_api.test1.Calculator();
+//        chap04_method_api.test2.Calculator c2 = new chap04_method_api.test2.Calculator();
 
         int ret1 = minNumberOf(10, 20);
         int ret2 = maxNumberOf(10, 20);
