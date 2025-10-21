c7c090a94fe13629f6a48c50fdb180370d3a8dae
ehsy25 <eyoun17@gmail.com>
2025-10-21 17:52:03 +0900

    notes and problem set 4 created

---
+++ 00_practice/11_YOUNHONGSUK/chap04-control-flow-statement-practice-question/src/main/java/com/ohgiraffers/level02/normal/Application3.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.level02.normal;
+
+import java.util.Scanner;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+
+        /* 임의의 숫자를 입력받아 해당 계절을 출력해주세요
+         * 단, 1~12사이의 숫자가 아닌 값이 들어는것은 "잘못 입력하셨습니다"를 출력
+         * 계절 구분
+         * 봄 : 3월,4월,5월
+         * 여름: 6월,7월,8월
+         * 가을: 9월,10월,11월
+         * 겨울: 12월,1월,2일
+         *
+         * -----  출력 -----
+         * 숫자를 입력하세요 : 1
+         * 겨울
+         * */
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a < 1 || a > 12){
+            System.out.println("invalid input");
+        }
+        else if(a == 3 || a == 4 || a == 5){
+            System.out.println("spring");
+        }
+        else if(a == 6 || a == 7 || a == 8){
+            System.out.println("summer");
+        }
+        else if(a == 9 || a == 10 || a == 11){
+            System.out.println("winter");
+        }
+        else if(a == 12 || a == 1 || a == 12){
+            System.out.println("winter");
+        }
+
+    }
+}
---
+++ 11_YOUNHONGSUK/chap04-control-flow-lecture-source/src/main/java/com/E/section02/looping/A_for.java
@@ -0,0 +1,57 @@
+package com.E.section02.looping;
+
+import java.util.Scanner;
+
+public class A_for {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void test(){
+        System.out.print("1 번째 학생의 이름을 입력해주세요 : ");
+        String student1 = sc.nextLine();
+        System.out.println("1 번째 학생의 이름은 " + student1 + "입니다.");
+
+        System.out.print("2 번째 학생의 이름을 입력해주세요 : ");
+        String student2 = sc.nextLine();
+        System.out.println("2 번째 학생의 이름은 " + student2 + "입니다.");
+
+        System.out.print("3 번째 학생의 이름을 입력해주세요 : ");
+        String student3 = sc.nextLine();
+        System.out.println("3 번째 학생의 이름은 " + student3 + "입니다.");
+
+        System.out.print("4 번째 학생의 이름을 입력해주세요 : ");
+        String student4 = sc.nextLine();
+        System.out.println("4 번째 학생의 이름은 " + student4 + "입니다.");
+
+        System.out.print("5 번째 학생의 이름을 입력해주세요 : ");
+        String student5 = sc.nextLine();
+        System.out.println("5 번째 학생의 이름은 " + student5 + "입니다.");
+
+        System.out.print("6 번째 학생의 이름을 입력해주세요 : ");
+        String student6 = sc.nextLine();
+        System.out.println("6 번째 학생의 이름은 " + student6 + "입니다.");
+
+        System.out.print("7 번째 학생의 이름을 입력해주세요 : ");
+        String student7 = sc.nextLine();
+        System.out.println("7 번째 학생의 이름은 " + student7 + "입니다.");
+
+        System.out.print("8 번째 학생의 이름을 입력해주세요 : ");
+        String student8 = sc.nextLine();
+        System.out.println("8 번째 학생의 이름은 " + student8 + "입니다.");
+
+        System.out.print("9 번째 학생의 이름을 입력해주세요 : ");
+        String student9 = sc.nextLine();
+        System.out.println("9 번째 학생의 이름은 " + student9 + "입니다.");
+
+        System.out.print("10 번째 학생의 이름을 입력해주세요 : ");
+        String student10 = sc.nextLine();
+        System.out.println("10 번째 학생의 이름은 " + student10 + "입니다.");
+
+        System.out.println("10명의 학생 이름을 입력 받고 출력하는 기능을 완료했습니다!");
+
+    }
+
+}
+
+
+
