766a2cbfa7a66f2afa8beca8d784e1e8eee53834
NoSa1e <cjsp1217@gmail.com>
2025-10-22 09:02:17 +0900

    solve

---
+++ 23_JUNGJAEWOO/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/B_ifelse.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section01.conditional;
+
+import java.util.Scanner;
+
+public class B_ifelse {
+
+    public void testSimpleIfElseStatement() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 하나 입력 : ");
+        int num = sc.nextInt();
+
+        if (num > 0){
+            if(num % 2 == 0) {
+                System.out.println("짝수입니다.");
+            } else {
+                System.out.println("홀수입니다.");
+            }
+        } else {
+            System.out.println("입력하신 숫자는 음수입니다. 양수를 입력해주세요.");
+        }
+    }
+}
