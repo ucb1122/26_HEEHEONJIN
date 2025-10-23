9a4b182a7af755a4ea0fe82041f1aacb891bd7c2
goergelee970115 <xwlgodx@gmail.com>
2025-10-24 08:48:22 +0900

    2025-10-23

---
+++ 18_LEEJIHEANG/chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Application3.java
@@ -0,0 +1,55 @@
+package com.ohgiraffers.section01.array;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+        int num; //지역 변수 전역변수는 JVM이 기본값을 넣어준다.
+//        System.out.println(num);
+        /*
+        * heap 영역에 할당될 경우 자료형에 따른 초기값이 설정되어 할당된다.
+        * 정수: 0
+        * 실수: 0.0
+        * 논리: false
+        * 문자: |u0000
+        * 참조: null
+        * */
+        int[] iarr = new int[10];
+        for(int i = 0; i<iarr.length; i++){
+            System.out.println("iarr[" + i + "] : " + iarr[i]);
+        }
+        String[] sarr = new String[10];
+        for(int i = 0; i<sarr.length; i++){
+            System.out.println("sarr[" + i + "] : " + sarr[i]);
+        }
+        char[] carr = new char[10];
+        for(int i = 0; i<carr.length; i++){
+            System.out.println("carr[" + i + "] : " + carr[i]);
+        }
+
+        boolean[] barr = new boolean[10];
+        for(int i = 0; i<barr.length; i++){
+            System.out.println("barr[" + i + "] : " + barr[i]);
+        }
+
+        /* 기본값 외의 값으로 초기화하면서 배열을 생성하고 싶을 때*/
+//        int[] iarr2 = new int[10]{11, 22, 33, 44, 55}
+        int[] iarr2 = {11, 22, 33, 44, 55};
+
+        int[] test = new int[5];
+        test[0] =11;
+        test[1] = 22;
+        test[2] = 33;
+        test[3] = 44;
+        test[4] = 55;
+        int[] test2 = {11, 22, 33, 44, 55};
+
+        int[] iarr3 = {11, 22, 33, 44, 55};
+        for(int i = 0; i<iarr3.length; i++){
+            System.out.println("iarr3[" + i + "] : " + iarr3[i]);
+        }
+        // 방은 선언된 개수만큼 만들어진다.
+        // 짜장면, 짬뽕, 탕수육, 삼겹살,
+        String[] sarr2 = {"짜장면","짬뽕","탕수육","삼겹살"};
+
+    }
+}
