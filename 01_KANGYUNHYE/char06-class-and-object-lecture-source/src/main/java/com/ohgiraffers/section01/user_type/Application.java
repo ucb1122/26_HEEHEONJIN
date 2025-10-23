c4b8b463a59bea9557f858677990eff6b2780c4f
ChoiKYH <priyune@naver.com>
2025-10-23 17:49:45 +0900

    chap06_update

---
+++ 01_KANGYUNHYE/char06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section01/user_type/Application.java
@@ -0,0 +1,45 @@
+package com.ohgiraffers.section01.user_type;
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) {
+        /*사용자 정의 자료형 사용하기*/
+
+        /* 변수 선언 및 객체 생성
+        * 클래스명 변수명 = new 크래스명(); <- 객체(instance)를 생성하는 구문이다.
+        * 객체를 생성하게 되면 클래스에 정의한 필드(전역변수)와 메소드 대로 객체가
+        * head 영역에 생성된다.
+         */
+        Member member = new Member();
+
+        System.out.println("=====> " + member.hashCode());
+        /*
+        * 필드에 접근하기 위해 래퍼런스변수명.필드명으로 접근한다.
+        * '.'은 참조 연산자이며 래퍼런스 변수가 참조하는 주소로 접근한다는 의미를 가진다.
+        * heap영역은 기본적으로 JVM초기값으로 초기화된다.
+        * */
+        System.out.println("member.id = " +  member.id);
+        System.out.println("member.pwd = " +  member.pwd);
+        System.out.println("member.name= " +  member.name);
+        System.out.println("member.age= " +  member.age);
+        System.out.println("member.gender= " +  member.gender);
+        System.out.println("member.hobby= " +  member.hobby);
+
+        member.id = "user01";
+        member.pwd = "pass";
+        member.name = "윤헤캉";
+        member.age = 25;
+        member.gender = 'F';
+        member.hobby = new String[]{"노래듣기","영화보기"};
+
+        System.out.println("member.id = " +  member.id);
+        System.out.println("member.pwd = " +  member.pwd);
+        System.out.println("member.name= " +  member.name);
+        System.out.println("member.age= " +  member.age);
+        System.out.println("member.gender= " +  member.gender);
+        System.out.println("member.hobby= " +  Arrays.toString(member.hobby));
+
+
+    }
+}
