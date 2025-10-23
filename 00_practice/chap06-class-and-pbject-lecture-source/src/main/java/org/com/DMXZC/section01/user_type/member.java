0508cbaf38fb74f96114ec3d2f871706a336f084
DMXZC <ysj7240@gmail.com>
2025-10-23 17:43:36 +0900

    완

---
+++ 00_practice/chap06-class-and-pbject-lecture-source/src/main/java/org/com/DMXZC/section01/user_type/member.java
@@ -0,0 +1,12 @@
+package org.com.DMXZC.section01.user_type;
+
+public class member {
+// 클래스 범위에 변수를 선언할 수 있으며, 이 변수를 전역 변수라고 한다.
+    String id;
+    String pwd;
+    String name;
+    int age;
+    char gender;
+    String[] hobby;
+
+}
