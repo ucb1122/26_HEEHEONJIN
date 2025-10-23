5fbe9df59ba77eb6ac15154ded9627f8847be038
NoSa1e <cjsp1217@gmail.com>
2025-10-23 17:53:28 +0900

    1023 solve

---
+++ 23_JUNGJAEWOO/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/test/Course.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.test;
+
+public class Course {
+    private String courseId;
+    private String title;
+    private int credit;
+
+    public void addStudent(Student s){}
+
+    public void removeStudent(Student s){}
+}
